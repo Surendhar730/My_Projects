fcf67428127e1c4c10b6c8cc121d622d94eb4dcb
Surendhar730 <119731960+Surendhar730@users.noreply.github.com>
2025-04-12 16:32:22 +0530

    Update SQL_Task_1_Joins.sql

--- SQL_Task_1_Joins.sql
+++ SQL_Task_1_Joins.sql
@@ -41,6 +41,18 @@ VALUES (5009, 'Rajamouli', 'Shoes', 20, '2025-04-06', 40000);
 INSERT INTO orders (OrderID, CustomerName, ProductName, Quantity, OrderDate, TotalAmount)
 VALUES (5010, 'Sukumar', 'Bat', 20, '2025-04-06', 50000);
 
+select * from Cricket_Sales;
+SaleID	ProductName	Quantity	Price	SaleDate
+10000	Sportsdress	50	100000.00	2025-04-12
+20000	Ball	    200	100000.00	2025-04-12
+30000	pads	    50	50000.00	2025-04-12
+40000	Stumps	    24	200000.00	2025-04-12
+50000	Bleaching powder	20	200000.00	2025-04-12
+60000	gloves	    20	10000.00	2025-04-12
+70000	Shoes	    80	100000.00	2025-04-12
+80000	Guards	    50	20000.00	2025-04-12
+90000	caps	    100	10000.00	2025-04-12
+
 
 CREATE TABLE Cricket_Sales (
     SaleID INT PRIMARY KEY,
@@ -71,8 +83,20 @@ VALUES (90000, 'caps', 100, 10000, '2025-04-12');
 INSERT INTO Cricket_Sales (SaleID, ProductName, Quantity, Price, SaleDate)
 VALUES (10000, 'Sportsdress', 50, 100000, '2025-04-12');
 
-select * from Cricket_Sales;
 select * from orders;
+
+OrderID	CustomerName	ProductName	Quantity	OrderDate	TotalAmount
+5000	Surendhar	     Bat	     10	     2025-04-05	     40000.00
+5001	Narendhar	     Ball	     20	     2025-04-06	     10000.00
+5002	Dharmendhar	    Gloves	     20	     2025-04-07	     10000.00
+5003	Arjun	        Ball	     20	     2025-04-06	     10000.00
+5004	Sarjanar	   Stumps	     12	     2025-04-06	     8000.00
+5005	Adhi	   Bleaching Powder	10	     2025-04-26	     10000.00
+5006	Vinay	       Bat	        20	     2025-04-04	     50000.00
+5007	Vijay	       Ball	        20	     2025-04-06	     10000.00
+5008	Jagun	       Pads	        20	     2025-04-06	     20000.00
+5009	Rajamouli	   Shoes	    20	     2025-04-06	     40000.00
+5010	Sukumar	       Bat	        20	     2025-04-06	     50000.00
 
 Delete from Cricket_Sales 
 WHERE SaleID IN(10003, 10004);
